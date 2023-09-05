f8fdd2c1a5d8102dd3a28a17189b62588a1c947b
Chibuike Iwuchukwu <chibuikeeugene@gmail.com>
2023-09-05 19:11:20 +0000

    created the dim customer model

--- models/mart/core/dim_customers.sql
+++ models/mart/core/dim_customers.sql
@@ -1,11 +1,48 @@
 with customers as (
-    select distinct(status) from {{ source('ecommerce', 'orders') }}
+    select * from {{ ref('stg_customers') }}
+),
+
+orders as (
+    select * from {{ ref('stg_orders') }}
+
+),
+
+customer_orders as (
+    select
+        customer_number,
+        customer_name,
+        contact_first_name,
+        contact_last_name,
+        credit_limit,
+        order_number
+
+    from customers
+    left join orders using (customer_number)
+    group by 1,2,3,4,5,6
+
+),
+
+successful_orders as (
+    select * from {{ ref('fct_orders_with_success') }}
+),
+
+final as (
+    select
+        customer_orders.customer_number as cust_no,
+        customer_orders.customer_name as cust_name,
+        customer_orders.contact_first_name as cust_f_name,
+        customer_orders.contact_last_name as cust_l_name,
+        customer_orders.credit_limit,
+        min(successful_orders.order_date) as first_order_dt,
+        max(successful_orders.order_date) as most_recent_order_dt,
+        coalesce(count(successful_orders.order_number), 0) as no_of_orders,
+        coalesce(round(sum(successful_orders.amount),1), 0) as clv
+
+
+    from customer_orders 
+    left join successful_orders using (order_number)
+    group by 1,2,3,4,5
+    order by clv desc
 )
 
-select * from customers
-
--- with failed_orders as (
---     select * from {{ source('ecommerce', 'orders') }} where status = 'Cancelled'
--- )
-
--- select * from failed_orders
+select * from final
